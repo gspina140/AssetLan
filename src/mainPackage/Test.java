4d96c9a3485a2e1b92b3f70bb7d87e42f57f36dd
Alberto <js.flakes46@gmail.com>
2022-04-07 15:50:53 -0700

    Cleaned Test.java, added comments

--- src/mainPackage/Test.java
+++ src/mainPackage/Test.java
@@ -1,76 +1,36 @@
 package mainPackage;
 
-import java.io.FileWriter;
 import java.io.FileInputStream;
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.*;
-
-import java.util.*;
 
 import parser.AssetLanLexer;
 import parser.AssetLanParser;
 import util.TestLexerListener;
 
 public class Test {
-    public static class MyParserListener extends BaseErrorListener {
-        @Override
-        public void syntaxError(Recognizer<?, ?> recognizer,
-                                Object offendingSymbol,
-                                int line, int charPositionInLine,
-                                String msg,
-                                RecognitionException e)
-        {
-            List<String> stack = ((Parser)recognizer).getRuleInvocationStack();
-            Collections.reverse(stack);
-            System.err.println("rule stack: "+stack+"\tRecognizer: Parser");
-            System.err.println("line "+line+":"+charPositionInLine+" at "+
-                                offendingSymbol+": "+msg);
-        }
-
-    }
-
-    public static class MyLexerListener extends BaseErrorListener {
-        @Override
-        public void syntaxError(Recognizer<?, ?> recognizer,
-                                Object offendingSymbol,
-                                int line, int charPositionInLine,
-                                String msg,
-                                RecognitionException e)
-        {
-            String outError = "line "+line+":"+charPositionInLine+": "+msg;
-
-            System.err.println("Recognizer: Lexer");
-            System.err.println("line "+line+":"+charPositionInLine+" at "+
-                                offendingSymbol+": "+msg);
-
-            try(FileWriter fw = new FileWriter("log.txt", true);
-                BufferedWriter bw = new BufferedWriter(fw);
-                PrintWriter out = new PrintWriter(bw))
-            {
-                out.println(outError);
-            } catch (IOException ioe) {
-                System.err.println("Error writing to file");
-            }
-        }
-    }
-
     public static void main(String[] args) throws Exception {
 
         String fileName = "input.al";   // We decided AssetLan extension to be "".al"
 
-		FileInputStream is = new FileInputStream(fileName);
-        CharStream input = CharStreams.fromStream(is);  // From Antlr4.6, ANTLRInputStream is deprecated, and CharStream is recommended instead
-        AssetLanLexer lexer = new AssetLanLexer(input);
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(new TestLexerListener());
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        AssetLanParser parser = new AssetLanParser(tokens);
-        parser.removeErrorListeners();  // remove ConsoleErrorListener
-        //parser.addErrorListener(new MyParserListener()); // add ours
-        parser.program();  // parse as usual
+		FileInputStream is = new FileInputStream(fileName);         // Program to compile is in a file
+        CharStream input = CharStreams.fromStream(is);              // From Antlr4.6 ANTLRInputStream is deprecated, CharStream is recommended instead
+        
+        /* Lexer */
+        AssetLanLexer lexer = new AssetLanLexer(input);             // Instantiate Lexer
+        lexer.removeErrorListeners();                               // Remove ConsoleErrorListener for lexer
+        lexer.addErrorListener(new TestLexerListener());            // Add new custom ErrorListener
+        CommonTokenStream tokens = new CommonTokenStream(lexer);    // Token stream
+
+        /* Parser */
+        AssetLanParser parser = new AssetLanParser(tokens);         // Instantiate Parser
+        parser.removeErrorListeners();                              // Remove ConsoleErrorListener for parser
+        
+        /* Visitor (semantic analysis) */
+		//AssetLanVisitorImpl visitor = new AssetLanVisitorImpl();    // Use custom visitor
+		//Node ast = visitor.visit(parser.prog());                    // Generazione AST 
+
+        parser.program();                                           // Parse as usual
     }
 }
