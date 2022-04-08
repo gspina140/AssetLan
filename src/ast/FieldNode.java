c8a96dac88c442a78816b9ffd30217cf57220b46
Alberto <js.flakes46@gmail.com>
2022-04-08 03:08:35 -0700

    Added Field Node, must implement Visit in AssetLanVisitorImpl

---
+++ src/ast/FieldNode.java
@@ -0,0 +1,45 @@
+package ast;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import util.Environment;
+import util.SemanticError;
+
+public class FieldNode implements Node {
+
+  private Node type;
+  private String id;
+  private Node exp;
+  
+  public ProgramNode (Node t, String i, Node e) {
+    type    = t;
+    id      = i;
+    exp     = e;
+  }
+  
+  public String toPrint(String s) {
+	return s+"Field\n" + type.toPrint(s+" ") + id + exp.toPrint(s+" ") ; 
+  }
+  
+  @Override
+	public ArrayList<SemanticError> checkSemantics(Environment env) {
+	    // Create result list
+  		ArrayList<SemanticError> res = new ArrayList<SemanticError>();
+  	  
+  		// env.offset = -2;
+  		HashMap<String,STentry> hm = env.symTable.get(env.nestingLevel);
+        STentry entry = new STentry(env.nestingLevel,type, env.offset--); // Introducing "entry"
+        
+        if ( hm.put(id,entry) != null )
+        		res.add(new SemanticError("Var id "+id+" already declared"));
+        
+        res.addAll(exp.checkSemantics(env));
+        
+        return res;
+	}
+  
+  //public Node typeCheck () {}
+  
+  //public String codeGeneration() {}
+
+}  
