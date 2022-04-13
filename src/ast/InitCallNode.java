5cb12f9799e5f4d667ade2f596cf6321ab2e4262
gppcaputo <103131712+gppcaputo@users.noreply.github.com>
2022-04-13 11:25:36 +0200

    Added CallNode, added InitCallNode
    da rivedere

---
+++ src/ast/InitCallNode.java
@@ -0,0 +1,67 @@
+package ast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import util.Environment;
+import util.SemanticError;
+
+public class InitCallNode implements Node {
+
+	private String id;
+	private ArrayList<Node> explist;
+	
+	public InitCallNode(String id) {
+		this.id = id;
+	
+	}
+	
+	@Override
+    public ArrayList<SemanticError> checkSemantics(Environment env){
+
+        //Create result list
+        ArrayList<SemanticError> res = new ArrayList<SemanticError>();
+        HashMap<String,STentry> hm = env.symTable.get(env.nestingLevel);
+        
+        if(hm.get(id) == null) {
+        	int nl = env.nestingLevel - 1;
+        	
+        	while(nl >= 0) {
+        		hm = env.symTable.get(nl);
+        		
+        		if(hm.get(id) == null) 
+        			nl--;
+        		else {
+        			for(Node e : explist) 
+                		res.addAll(e.checkSemantics(env));
+                	
+        			return res;
+        		}
+        		if(nl < 0) {
+        			res.add(new SemanticError("Function id" + id + "has not been declared"));
+        		}
+        		
+        	}
+       
+        	
+        }else {
+        	for(Node e : explist) {
+        		res.addAll(e.checkSemantics(env));
+        	}
+        }
+        return res;
+	}
+	
+	public void addExp(Node n) {
+		explist.add(n);
+	}
+
+	
+	@Override
+	public String toPrint(String indent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
