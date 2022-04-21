69766b8480db8ad3b05d01a46598da3c3d2bfeb7
Alberto <js.flakes46@gmail.com>
2022-04-21 02:26:05 -0700

    Added documentation to SemanticError class

--- src/util/SemanticError.java
+++ src/util/SemanticError.java
@@ -2,16 +2,27 @@ package util;
 
 public class SemanticError {
 	
+	/**
+	 * A string containing the error message
+	 */
 	public final String msg;
 	
+	/**
+	 * Class constructor
+	 * @param String msg the error message
+	 * @return SemanticError an object of type SemanticError with error string msg
+	 */
 	public SemanticError(String msg) {
 		this.msg = msg;
 	}
 	
+	/**
+	 * Override of the function to cast from SemanticError to String
+	 * @param void
+	 * @return String a string containing the error message
+	 */
 	@Override
-	public String toString() {
-		
+	public String toString() {		
 		return msg;
-	}
-	
+	}	
 }
